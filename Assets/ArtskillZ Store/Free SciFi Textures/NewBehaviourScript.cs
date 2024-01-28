27aa6cb3145dad39a8d5012024da23120ce4135a
Jack2024-hub <jacklentini4@gmail.com>
2024-01-28 07:42:18 -0500

    this is dumb

--- Assets/ArtskillZ Store/Free SciFi Textures/NewBehaviourScript.cs
+++ Assets/ArtskillZ Store/Free SciFi Textures/NewBehaviourScript.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class monsterspawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	(System.Serializable)
+	public class WaveContent
+	{
+		(SerializedField)(NonReorderable) GameObject() monsterSpawner();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+	(SerializedField) (NonReorderable) WaveContent() waves;
